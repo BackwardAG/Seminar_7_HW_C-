b45b5c9e12ee4d0454fab469ea3108bb1939c71f
Armen G <ar.gukasov@gmail.com>
2023-02-08 21:41:44 +0300

    HW seminar7 3 tasks added

---
+++ task2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, 
+// возвращает индексы этого элемента в двумерном массиве или же указание,
+//  что такого числа нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+int[,] FillMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 100);
+        }
+    }
+
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+int[,] matrix = FillMatrix(3, 4);
+PrintMatrix(matrix);
+System.Console.WriteLine("Enter a number: ");
+int userinput = Convert.ToInt32(Console.ReadLine());
+bool search = false;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (userinput == matrix[i, j])
+        {
+            System.Console.WriteLine($"Данное значение находится по адресу [{i}],[{j}]");
+            search = true;
+            break;
+        }
+    }
+    if (!search)
+    {
+        System.Console.WriteLine($"Данного элемента нет в строке {i}!");
+
+    }
+
+}
+
---
+++ task3/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 100);
+        }
+    }
+
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+System.Console.WriteLine("Введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillMatrix(n,m);
+PrintMatrix(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = Math.Round(avarage / n,2);
+    Console.Write(avarage + "; ");
+}
+
+Console.WriteLine();
+
+
+
+
+
